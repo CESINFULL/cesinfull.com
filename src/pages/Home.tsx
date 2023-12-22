4edff262695808dad0ac4bd67137c1c98dbbbb49
Miguel-Dorta <contact@migueldorta.com>
2023-12-22 12:57:45 +0000

    Add Hero

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import NavBar from '../components/NavBar';
+import Hero from '../components/Hero';
 
 const Home = () => {
   return (
     <>
       <NavBar />
-      <h1>Home</h1>
+      <Hero />
     </>
   );
 };
