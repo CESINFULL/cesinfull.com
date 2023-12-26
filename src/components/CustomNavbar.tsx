e4ef01da287953ef80b46ac5b85155246df73f63
FebeFarina <alu0101430973@ull.edu.es>
2023-12-26 11:54:51 +0000

    Who whe are finished + navbar fix

--- src/components/CustomNavbar.tsx
+++ src/components/CustomNavbar.tsx
@@ -3,15 +3,19 @@ import CesinfLogo from './svg/CesinfLogo';
 
 export default function CustomNavbar() {
   return (
-    <Navbar fluid rounded className='fixed w-full'>
+    <Navbar fluid rounded className="fixed w-full bg-gray-800 z-30">
       <Navbar.Brand href="/">
-        <CesinfLogo letterColor='#ffffff' circleColor='#ffffff' className='ml-3 md:ml-0 mr-3 h-10 sm:h-9' />
-        <span className="hidden sm:inline self-center whitespace-nowrap text-xl font-semibold dark:text-white">CESINF IX</span>
+        <CesinfLogo
+          letterColor="#ffffff"
+          circleColor="#ffffff"
+          className="ml-3 md:ml-0 mr-3 h-10 sm:h-9"
+        />
+        <span className="hidden sm:inline self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          CESINF IX
+        </span>
       </Navbar.Brand>
       <div className="flex md:order-2">
-        <Button gradientDuoTone="purpleToBlue">
-          Compra tu entrada
-        </Button>
+        <Button gradientDuoTone="purpleToBlue">Compra tu entrada</Button>
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
