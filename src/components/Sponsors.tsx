22a25dbf3cb6f0934e01cffea181fa12e2df8b26
guillex387 <guillesiglez387@gmail.com>
2024-01-08 12:58:16 +0000

    Added adjustments

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -4,15 +4,17 @@ import ecovium from '../images/ecovium_sponsor.webp';
 export default function Sponsors() {
   return (
     <>
-      <h3 className="text-[7vw] sm:text-5xl text-center pb-2 font-bold">PATROCINADORES</h3>
-      <section className="grid grid-cols-1 lg:grid-cols-2 gap-20 place-items-center pt-12 pb-32">
+      <h3 className="text-[7vw] sm:text-5xl text-center pb-8 font-bold">
+        PATROCINADORES
+      </h3>
+      <section className="grid grid-cols-1 lg:grid-cols-2 gap-10 sm:gap-20 place-items-center pb-24">
         <img
-          className="max-w-[400px] max-h-[400px] rounded"
+          className="p-2 sm:p-0 sm:max-w-[400px] max-h-[300px] rounded"
           src={ecovium}
           alt="Foto del evento"
         />
         <img
-          className="max-w-[400px] max-h-[300px]"
+          className="p-2 sm:p-0 sm:max-w-[400px] sm:max-h-[300px]"
           src={lean_mind}
           alt="Foto del evento"
         />
