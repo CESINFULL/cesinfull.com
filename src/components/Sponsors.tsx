2067defa49b4abc9ce60c2fe4bc55c2aad38a3db
guillex387 <guillesiglez387@gmail.com>
2024-01-08 11:49:55 +0000

    Added the new sponsors

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -1,67 +1,22 @@
-import unknown from '../images/unkwown_sponsor.webp';
+import lean_mind from '../images/lean-mind_sponsor.webp';
+import ecovium from '../images/ecovium_sponsor.webp';
 
 export default function Sponsors() {
   return (
     <>
-      <h1 className='text-5xl text-center pb-2 font-bold'>PATROCINADORES</h1>
-      <div className='mx-40 lg:flex lg:place-content-evenly items-center pb-32'>
-        <div className='flex flex-col items-center text-center'>
-          <img 
-            className='mt-12'
-            src={unknown}
-            alt='Foto del evento' 
-            width='200'
-          />
-          <img 
-            className='mt-12'
-            src={unknown}
-            alt='Foto del evento' 
-            width='200'
-          />
-        </div>
-        <div className='flex flex-col items-center text-center'>
-          <img 
-            className='mt-12'
-            src={unknown}
-            alt='Foto del evento' 
-            width='200'
-          />
-          <img 
-            className='mt-12'
-            src={unknown}
-            alt='Foto del evento' 
-            width='200'
-          />
-        </div>
-        <div className='flex flex-col items-center text-center'>
-          <img 
-            className='mt-12'
-            src={unknown}
-            alt='Foto del evento' 
-            width='200'
-          />
-          <img 
-            className='mt-12'
-            src={unknown}
-            alt='Foto del evento' 
-            width='200'
-          />
-        </div>
-        <div className='flex flex-col items-center text-center'>
-          <img 
-            className='mt-12'
-            src={unknown}
-            alt='Foto del evento' 
-            width='200'
-          />
-          <img 
-            className='mt-12'
-            src={unknown}
-            alt='Foto del evento' 
-            width='200'
-          />
-        </div>
-      </div>
+      <h1 className="text-5xl text-center pb-2 font-bold">PATROCINADORES</h1>
+      <section className="grid grid-cols-1 lg:grid-cols-2 gap-20 place-items-center pt-12 pb-32">
+        <img
+          className="max-w-[400px] max-h-[400px] rounded"
+          src={ecovium}
+          alt="Foto del evento"
+        />
+        <img
+          className="max-w-[400px] max-h-[300px]"
+          src={lean_mind}
+          alt="Foto del evento"
+        />
+      </section>
     </>
   );
 }
