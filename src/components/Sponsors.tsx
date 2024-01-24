86f266a54784f0f700315737182c2944ed993a51
Miguel-Dorta <contact@migueldorta.com>
2024-01-24 00:00:06 +0000

    Add inetel and sort sponsors

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -4,6 +4,7 @@ import catedraCaja from '../images/catedra-bob.webp';
 import catedraSeguridad from '../images/catedra-ciberseguridad-ull.webp';
 import catedraEdosoft from '../images/catedra-edosoft-ull.webp';
 import gdgTenerife from '../images/gdg-tenerife.webp';
+import inetel from '../images/inetel.webp';
 import politecnica from '../images/politecnica-ull.webp';
 import pythonCanarias from '../images/python-canarias.webp';
 import sao from '../images/sao.webp';
@@ -15,67 +16,74 @@ export default function Sponsors() {
         PATROCINADORES
       </h3>
       <section className="grid grid-cols-1 lg:grid-cols-2 gap-10 sm:gap-20 place-items-center pb-24">
-        <a href="https://ecovium.com/en/">
-          <img
-            className="p-2 sm:p-0 sm:max-w-[400px] w-[350px] max-h-[300px] rounded"
-            src={ecovium}
-            alt="Foto del evento"
-          />
-        </a>
-        <a href="https://leanmind.es/es/">
-          <img
-            className="p-2 sm:p-0 sm:max-w-[400px] w-[350px] sm:max-h-[300px]"
-            src={lean_mind}
-            alt="Foto del evento"
-          />
-        </a>
         <a href="https://www.ull.es/catedras/catedrabob/">
           <img
             className="p-2 sm:p-0 sm:max-w-[400px] w-[350px] sm:max-h-[300px]"
             src={catedraCaja}
-            alt="Catedra Caja BOB"
-          />
-        </a>
-        <a href="https://www.ull.es/catedras/catedra-ciberseguridad-binter/">
-          <img
-            className="p-2 sm:p-0 sm:max-w-[400px] w-[350px] sm:max-h-[300px]"
-            src={catedraSeguridad}
-            alt=""
+            alt="Cátedra Cajasiete Big Data, Open Data y Blockchain"
           />
         </a>
         <a href="https://www.ull.es/catedras/catedra-edosoft-computacion-nube/">
           <img
             className="p-2 sm:p-0 sm:max-w-[400px] w-[350px] sm:max-h-[300px]"
             src={catedraEdosoft}
-            alt=""
+            alt="Cátedra Edosoft de Computación en la Nube e Inteligencia Artificial"
           />
         </a>
-        <a href="https://gdg.community.dev/gdg-tenerife/">
+        <a href="https://www.ull.es/catedras/catedra-ciberseguridad-binter/">
           <img
-            className="p-2 sm:p-0 sm:max-w-[400px] w-[350px] sm:max-h-[300px] bg-white rounded"
-            src={gdgTenerife}
-            alt=""
+            className="p-2 sm:p-0 sm:max-w-[400px] w-[350px] sm:max-h-[300px]"
+            src={catedraSeguridad}
+            alt="Cátedra en Ciberseguridad Binter"
+          />
+        </a>
+        <a href="https://ecovium.com/en/">
+          <img
+            className="p-2 sm:p-0 sm:max-w-[400px] w-[350px] max-h-[300px] rounded"
+            src={ecovium}
+            alt="Ecovium"
           />
         </a>
         <a href="https://www.ull.es/centros/escuela-politecnica-superior-de-ingenieria/">
           <img
             className="p-2 sm:p-0 sm:max-w-[400px] w-[350px] sm:max-h-[300px] bg-white rounded"
             src={politecnica}
-            alt=""
+            alt="Escuela Politécnica Superior de Ingeniería"
+          />
+        </a>
+        <a href="https://gdg.community.dev/gdg-tenerife/">
+          <img
+            className="p-2 sm:p-0 sm:max-w-[400px] w-[350px] sm:max-h-[300px] bg-white rounded"
+            src={gdgTenerife}
+            alt="GDG Tenerife"
+          />
+        </a>
+        <a href="https://www.grupoinetel.com/">
+          <img
+            className="p-2 sm:p-0 sm:max-w-[400px] w-[350px] sm:max-h-[300px] bg-white rounded"
+            src={inetel}
+            alt="Inetel"
+          />
+        </a>
+        <a href="https://leanmind.es/es/">
+          <img
+            className="p-2 sm:p-0 sm:max-w-[400px] w-[350px] sm:max-h-[300px]"
+            src={lean_mind}
+            alt="Lean Mind"
           />
         </a>
         <a href="https://pythoncanarias.es/">
           <img
             className="p-2 sm:p-0 sm:max-w-[400px] w-[350px] sm:max-h-[300px]"
             src={pythonCanarias}
-            alt=""
+            alt="Python Canarias"
           />
         </a>
         <a href="https://saosl.com/">
           <img
             className="p-2 sm:p-0 sm:max-w-[400px] w-[350px] sm:max-h-[300px]"
             src={sao}
-            alt=""
+            alt="SAO"
           />
         </a>
       </section>
